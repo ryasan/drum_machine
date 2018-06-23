9d196a01c629d9c0b4d034981ac97d72984820f9
Ryan Santos <ryansantos86@gmail.com>
2018-06-22 19:27:23 -0700

    commit

--- src/components/DrumPadItem/DrumPadItem.jsx
+++ src/components/DrumPadItem/DrumPadItem.jsx
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 export default class DrumPadItem extends Component {
   playSound(id) {
     const sound = document.getElementById(id);
-    sound.currentTime = 0; 
+    sound.currentTime = 0;
     sound.play();
   }
 
   render() {
-    const { title, keyTrigger, url } = this.props.drumPad;
+    const { keyTrigger, url } = this.props.drumPad;
     return (
       <div className="drum-pad" onClick={() => this.playSound(keyTrigger)}>
         {keyTrigger}
