e8505db3707207075ea8d799036d45c201909677
Ryan Santos <ryansantos86@gmail.com>
2018-06-24 21:17:07 -0700

    (update) power button styling

--- src/components/DrumPad/DrumPad.jsx
+++ src/components/DrumPad/DrumPad.jsx
@@ -3,16 +3,16 @@ import React, { Component } from 'react';
 export default class DrumPad extends Component {
   render() {
     const { title, keyTrigger, url } = this.props.drumPad;
-    const classes = this.props.activePad === keyTrigger ? 'drum-pad active' : 'drum-pad';
+    const classes =
+      this.props.activePad === keyTrigger ? 'drum-pad active' : 'drum-pad';
     return (
       <div
         className={classes}
+        id={title}
         onClick={() => this.props.onPlaySound(keyTrigger, title)}
       >
         {keyTrigger}
-        <audio id={keyTrigger} className="clip">
-          <source src={url} type="audio/wav" />
-        </audio>
+        <audio id={keyTrigger} src={url} className="clip" type="audio/wav" />
       </div>
     );
   }
--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import './Switch.css';
 
 export default class Switch extends Component {
+  constructor() {
+    super();
+    this.state = {
+      powerOn: true
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange() {
+    this.setState({ powerOn: !this.state.powerOn });
+  }
+
   render() {
     return (
       <div className="row switch-container">
-        <div className="switch">
-          <label>
-            power
-            <input type="checkbox" />
-          </label>
-        </div>
+        <div className="switch-title">power</div>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={this.state.powerOn}
+            onChange={this.handleInputChange}
+          />
+          <span className="slider" />
+        </label>
       </div>
     );
   }
